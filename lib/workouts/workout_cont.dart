8eb4c538a3b8f2c286d5d163e9ff507f4803b840
Tolibzhon <sidikovtolibjon2000@gmail.com>
2024-03-23 12:53:37 +0600

    add change

--- lib/workouts/workout_cont.dart
+++ lib/workouts/workout_cont.dart
@@ -274,23 +274,33 @@ List<WorkoutsCont> listGym = [
             '''Stand with your feet shoulder-width apart, then squat down and place your hands on the ground. Jump your feet back into a plank position, perform a push-up if desired, then jump your feet back towards.''',
       ),
       Podr(
-        title: '',
-        min: 0,
-        kcal: 0,
-        dis: '''''',
+        title: 'Squats:',
+        min: 5,
+        kcal: 150,
+        dis: '''
+Stand with your feet hip-width apart and toes slightly turned out. Lower your body by bending your knees and pushing your hips back, as if you're sitting back into a chair. Keep your chest up and core engaged.''',
       ),
       Podr(
-        title: '',
-        min: 0,
-        kcal: 0,
-        dis: '''''',
+        title: 'Push-Ups:',
+        min: 5,
+        kcal: 150,
+        dis:
+            '''Start in a plank position with your hands slightly wider than shoulder-width apart. Lower your body towards the ground by bending your elbows, then push back up to the starting position.''',
       ),
       Podr(
-        title: '',
-        min: 0,
-        kcal: 0,
-        dis: '''''',
+        title: 'Step-Ups:',
+        min: 5,
+        kcal: 150,
+        dis:
+            '''Stand in front of a sturdy bench or step. Step up onto the bench with one foot, pressing through the heel to lift your body up. Bring the other foot to meet it, then step back down with the same foot leading.''',
       ),
+      Podr(
+        title: 'Jumping Jacks:',
+        min: 5,
+        kcal: 150,
+        dis:
+            '''Stand with your feet together and arms by your sides. Jump your feet out to the sides while simultaneously raising your arms overhead. Jump back to the starting position and repeat for a set duration or number of repetitions.''',
+      )
     ],
   ),
   WorkoutsCont(
@@ -299,40 +309,46 @@ List<WorkoutsCont> listGym = [
     min: 27,
     podr: [
       Podr(
-        title: '',
-        min: 0,
-        kcal: 0,
-        dis: '''''',
+        title: 'Treadmill Running:',
+        min: 6,
+        kcal: 100,
+        dis: '''
+Start by adjusting the speed and incline settings according to your fitness level. Begin with a warm-up walk, then gradually increase the speed to a comfortable running pace. Maintain good posture with your chest up and core engaged.''',
       ),
       Podr(
-        title: '',
-        min: 0,
-        kcal: 0,
-        dis: '''''',
+        title: 'Stationary Bike:',
+        min: 5,
+        kcal: 150,
+        dis: '''
+Adjust the seat and handlebar height to a comfortable position. Start pedaling at a moderate pace, gradually increasing the resistance as needed to increase the intensity. ''',
       ),
       Podr(
-        title: '',
-        min: 0,
-        kcal: 0,
-        dis: '''''',
+        title: 'Elliptical Trainer:',
+        min: 5,
+        kcal: 180,
+        dis: '''
+Adjust a bench to a 30-45 degree incline. Lie back on the bench with a dumbbell in each hand, arms extended straight up above your chest. Lower the dumbbells down towards your chest, then press them back up to the starting position.''',
       ),
       Podr(
-        title: '',
-        min: 0,
-        kcal: 0,
-        dis: '''''',
+        title: 'Rowing Machine:',
+        min: 5,
+        kcal: 120,
+        dis: '''Rowing Machine (Row Ergometer):
+Sit on the seat with your feet securely strapped in the footrests. Grasp the handle with an overhand grip and extend your arms forward. Push through your legs while simultaneously pulling the handle towards your chest.''',
       ),
       Podr(
-        title: '',
-        min: 0,
-        kcal: 0,
-        dis: '''''',
+        title: 'Arc Trainer:',
+        min: 2,
+        kcal: 100,
+        dis: '''
+Step onto the pedals and grasp the handles. Begin moving your legs in a smooth, elliptical motion while simultaneously pushing and pulling the handles back and forth. Adjust the resistance level and incline to vary the intensity.''',
       ),
       Podr(
-        title: '',
-        min: 0,
-        kcal: 0,
-        dis: '''''',
+        title: 'Circuit Training:',
+        min: 2,
+        kcal: 100,
+        dis: '''
+Set up a circuit of cardio machines or bodyweight exercises at different stations in the gym. Perform each exercise for a set duration or number of repetitions before moving on to the next station. I''',
       ),
     ],
   ),
@@ -342,10 +358,39 @@ List<WorkoutsCont> listGym = [
     min: 24,
     podr: [
       Podr(
-        title: '',
-        min: 0,
-        kcal: 0,
-        dis: '''''',
+        title: 'Bodyweight Squats:',
+        min: 5,
+        kcal: 150,
+        dis: '''
+Stand with your feet shoulder-width apart. Lower your body by bending your knees and pushing your hips back, keeping your chest up and core engaged. Lower down until your thighs are parallel to the ground.''',
+      ),
+      Podr(
+        title: 'Dumbbell Lunges:',
+        min: 5,
+        kcal: 180,
+        dis: '''
+Hold a dumbbell in each hand, palms facing in. Step forward with one foot and lower your body until both knees are bent at a 90-degree angle. Push through the heel of your front foot to return to the starting position.''',
+      ),
+      Podr(
+        title: 'Push-Ups:',
+        min: 5,
+        kcal: 160,
+        dis: '''
+Start in a plank position with your hands slightly wider than shoulder-width apart. Lower your body towards the ground by bending your elbows, then push back up to the starting position.''',
+      ),
+      Podr(
+        title: 'Plank:',
+        min: 5,
+        kcal: 90,
+        dis: '''
+Get into a push-up position with your hands directly under your shoulders and your body forming a straight line from head to heels. Hold this position, engaging your core muscles. You can also try side planks to target obliques.''',
+      ),
+      Podr(
+        title: 'Jumping Jacks:',
+        min: 4,
+        kcal: 100,
+        dis: '''
+Stand with your feet together and arms by your sides. Jump your feet out to the sides while simultaneously raising your arms overhead. Jump back to the starting position and repeat for a set duration or number of repetitions.''',
       ),
     ],
   ),
@@ -355,10 +400,32 @@ List<WorkoutsCont> listGym = [
     min: 22,
     podr: [
       Podr(
-        title: '',
-        min: 0,
-        kcal: 0,
-        dis: '''''',
+        title: 'Foam Rolling:',
+        min: 3,
+        kcal: 130,
+        dis: '''
+Use a foam roller to target major muscle groups such as the calves, hamstrings, quadriceps, glutes, back, and shoulders. Roll slowly over each muscle group, pausing on any tight or tender areas and applying gentle pressure.''',
+      ),
+      Podr(
+        title: 'Hip Flexor Stretch:',
+        min: 3,
+        kcal: 150,
+        dis: '''
+Kneel on one knee with the other foot flat on the ground in front of you. Engage your core and gently push your hips forward until you feel a stretch in the front of your hip and thigh.''',
+      ),
+      Podr(
+        title: 'Hamstring Stretch:',
+        min: 4,
+        kcal: 180,
+        dis: '''
+Sit on the floor with one leg extended straight in front of you and the other leg bent with the sole of the foot against the inner thigh of the extended leg. Hinge forward at the hips, reaching towards your toes while keeping your back straight.''',
+      ),
+      Podr(
+        title: "Child's Pose:",
+        min: 2,
+        kcal: 120,
+        dis: '''
+Start on your hands and knees with your toes together and knees apart. Sit back onto your heels, reaching your arms forward and lowering your chest towards the ground.''',
       ),
     ],
   ),
@@ -368,10 +435,60 @@ List<WorkoutsCont> listGym = [
     min: 30,
     podr: [
       Podr(
-        title: '',
-        min: 0,
-        kcal: 0,
-        dis: '''''',
+        title: 'Barbell Squats:',
+        min: 3,
+        kcal: 100,
+        dis: '''
+Start by standing with your feet shoulder-width apart, holding a barbell across your upper back. Engage your core and lower your body by bending your knees and pushing your hips back, keeping your chest up and back straight.''',
+      ),
+      Podr(
+        title: 'Dumbbell Bench Press:',
+        min: 4,
+        kcal: 150,
+        dis: '''
+Lie on a flat bench with a dumbbell in each hand, palms facing forward, and arms extended above your chest. Lower the dumbbells towards your chest, keeping your elbows at a 45-degree angle to your body.''',
+      ),
+      Podr(
+        title: 'Deadlifts:',
+        min: 3,
+        kcal: 180,
+        dis: '''
+Stand with your feet hip-width apart, holding a barbell or dumbbells in front of your thighs with an overhand grip. Hinge at the hips and lower the weights towards the ground while keeping your back flat and chest lifted.''',
+      ),
+      Podr(
+        title: 'Pull-Ups:',
+        min: 5,
+        kcal: 120,
+        dis: '''
+Grab a pull-up bar with an overhand grip, hands slightly wider than shoulder-width apart. Hang with your arms fully extended, then pull your body up until your chin is above the bar. Lower yourself back down with control.''',
+      ),
+      Podr(
+        title: 'Plank:',
+        min: 2,
+        kcal: 100,
+        dis: '''
+Get into a push-up position with your hands directly under your shoulders and your body forming a straight line from head to heels. Hold this position, engaging your core muscles. You can also try side planks to target obliques.''',
+      ),
+      Podr(
+        title: 'Dumbbell Shoulder Press:',
+        min: 6,
+        kcal: 100,
+        dis: '''
+Sit on a bench with a backrest or stand with your feet shoulder-width apart, holding a dumbbell in each hand at shoulder height with your palms facing forward. Press the weights overhead until your arms are fully extended.''',
+      ),
+      Podr(
+        title: 'Mountain Climbers:',
+        min: 3,
+        kcal: 100,
+        dis: '''
+Start in a plank position, then alternate bringing your knees towards your chest in a running motion. Keep your core engaged throughout the exercise.''',
+      ),
+      Podr(
+        title: 'Dumbbell Rows:',
+        min: 4,
+        kcal: 100,
+        dis: '''
+Hold a dumbbell in each hand and hinge forward at the hips, keeping your back flat and knees slightly bent. Pull the dumbbells up towards your ribcage, squeezing your shoulder blades together at the top of the movement.''',
       ),
     ],
   ),
---
+++ lib/workouts/workuot_start.dart
@@ -0,0 +1,328 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:weightburner_119/core/con_bar.dart';
+import 'package:weightburner_119/core/wb_colors.dart';
+import 'package:weightburner_119/core/wb_motin.dart';
+import 'package:weightburner_119/workouts/widgets/activity_widget.dart';
+import 'package:weightburner_119/workouts/workout_cont.dart';
+
+class WorkoutStart extends StatefulWidget {
+  const WorkoutStart({
+    super.key,
+    required this.model,
+  });
+  final WorkoutsCont model;
+  @override
+  State<WorkoutStart> createState() => _WorkoutStartState();
+}
+
+class _WorkoutStartState extends State<WorkoutStart> {
+  final PageController controller = PageController();
+  int currantPage = 0;
+
+  Timer timerStart = Timer(Duration.zero, () {});
+  late final oneSecStart = const Duration(seconds: 1);
+  bool isChekStart = false;
+
+  bool _isRunning = false;
+  late int totalSecondsMain;
+
+  int exercises = 0;
+  int calories = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    sssavihb();
+    startTimerStart();
+    totalSecondsMain = widget.model.podr[currantPage].min * 60;
+    controller.addListener(() {
+      if (controller.page?.round() != currantPage) {
+        handlePageChange(controller.page!.round());
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    timerMain.cancel();
+    controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> sssavihb() async {
+    int caloriesgetCalories = await getCalories();
+    setState(() {
+      calories = caloriesgetCalories;
+    });
+  }
+
+  int secondsStart = 0;
+  Future<void> startTimerStart() async {
+    secondsStart = 3;
+    timerStart = Timer.periodic(
+      oneSecStart,
+      (Timer timer) {
+        if (mounted) {
+          setState(() {
+            secondsStart--;
+            if (secondsStart == 0) {
+              timer.cancel();
+              setState(() {
+                isChekStart = true;
+              });
+              if (!_isRunning) {
+                if (mounted) {
+                  startTimerMain();
+                  setState(() {
+                    _isRunning = !_isRunning;
+                  });
+                }
+              }
+            }
+          });
+        }
+      },
+    );
+  }
+
+  String minutesConvert(int seconds) {
+    Duration clockTimer = Duration(seconds: seconds);
+    return clockTimer.inSeconds.remainder(60).toString().padLeft(
+          2,
+        );
+  }
+
+  Timer timerMain = Timer(Duration.zero, () {});
+  late final oneSecMain = const Duration(seconds: 1);
+  late int secondMain = totalSecondsMain;
+  double progress = 0;
+
+  Future<void> startTimerMain() async {
+    if (!_isRunning) {
+      secondMain = totalSecondsMain;
+
+      _isRunning = true;
+
+      timerMain = Timer.periodic(
+        oneSecMain,
+        (Timer timer) {
+          if (mounted) {
+            setState(() {
+              secondMain--;
+              progress = (totalSecondsMain - secondMain) / totalSecondsMain;
+              if (secondMain <= 0) {
+                timer.cancel();
+                _isRunning = false;
+                if (currantPage < widget.model.podr.length - 1) {
+                  controller.nextPage(
+                      duration: const Duration(milliseconds: 300),
+                      curve: Curves.easeIn);
+                } else {}
+              }
+            });
+          }
+        },
+      );
+    }
+  }
+
+  String minutesConvertMain(int seconds) {
+    Duration clockTimer = Duration(seconds: seconds);
+    return '${clockTimer.inMinutes.remainder(60).toString().padLeft(2, '0')}:${clockTimer.inSeconds.remainder(60).toString().padLeft(2, '0')}';
+  }
+
+  void handlePageChange(int newPageIndex) {
+    if (timerMain.isActive) {
+      timerMain.cancel();
+    }
+
+    setState(() {
+      currantPage = newPageIndex;
+      totalSecondsMain = widget.model.podr[currantPage].min * 60;
+      secondMain = totalSecondsMain;
+      progress = 0;
+      _isRunning = false;
+    });
+
+    startTimerMain();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: WbColors.blue04213E,
+      appBar: AppBar(
+        backgroundColor: WbColors.blue04213E,
+        scrolledUnderElevation: 0,
+        leading: const BackButton(color: Colors.white),
+        title: Text(
+          'Workout',
+          style: TextStyle(
+            fontSize: 24.h,
+            fontWeight: FontWeight.w600,
+            color: WbColors.white,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.r),
+          child: Column(
+            children: [
+              SizedBox(height: 12.h),
+              Image.asset(widget.model.image),
+              SizedBox(height: 8.h),
+              Row(
+                children: [
+                  const Spacer(),
+                  Text(
+                    currantPage == widget.model.podr.length - 1
+                        ? ''
+                        : 'Rest 30 seconds',
+                    style: TextStyle(
+                      fontSize: 18.h,
+                      fontWeight: FontWeight.w600,
+                      color: WbColors.white,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 12.h),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.all(12.h),
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: const Color(0xff003D79),
+                    borderRadius: BorderRadius.circular(16),
+                  ),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 60,
+                        backgroundColor: Colors.white,
+                        child: CircularPercentIndicator(
+                          radius: 60.0,
+                          lineWidth: 3.0,
+                          percent: progress,
+                          center: Text(
+                            minutesConvertMain(secondMain),
+                            style: TextStyle(
+                              color: WbColors.black,
+                              fontWeight: FontWeight.w600,
+                              fontSize: 30.h,
+                            ),
+                          ),
+                          progressColor: WbColors.blue009AFF,
+                          backgroundColor: const Color(0xff003D79),
+                        ),
+                      ),
+                      SizedBox(height: 24.h),
+                      Expanded(
+                        child: PageView(
+                          onPageChanged: (value) {
+                            setState(() {
+                              handlePageChange(value);
+                            });
+                          },
+                          controller: controller,
+                          children:
+                              List.generate(widget.model.podr.length, (index) {
+                            return Padding(
+                              padding: EdgeInsets.symmetric(horizontal: 4.r),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    widget.model.podr[currantPage].title,
+                                    style: TextStyle(
+                                      fontSize: 14.h,
+                                      fontWeight: FontWeight.w400,
+                                      color: WbColors.white,
+                                    ),
+                                  ),
+                                  Text(
+                                    widget.model.podr[currantPage].dis,
+                                    style: TextStyle(
+                                      color: WbColors.white,
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 14.h,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          }),
+                        ),
+                      ),
+                      SizedBox(height: 12.h),
+                      WbMotion(
+                        onPressed: () async {
+                          if (currantPage == widget.model.podr.length - 1) {
+                            timerMain.cancel();
+                            Navigator.pushAndRemoveUntil(
+                              context,
+                              PageRouteBuilder(
+                                pageBuilder: (_, __, ___) =>
+                                    const WbBottomBar(),
+                                transitionsBuilder: (context, animation,
+                                    secondaryAnimation, child) {
+                                  const begin = Offset(-1.0, 0.0);
+                                  const end = Offset.zero;
+                                  const curve = Curves.easeInOut;
+                                  var tween = Tween(begin: begin, end: end)
+                                      .chain(CurveTween(curve: curve));
+                                  var offsetAnimation = animation.drive(tween);
+                                  return SlideTransition(
+                                      position: offsetAnimation, child: child);
+                                },
+                              ),
+                              (route) => false,
+                            );
+                            calories =
+                                calories + widget.model.podr[currantPage].kcal;
+                            await setCalories(calories);
+                          } else {
+                            controller.nextPage(
+                              duration: const Duration(milliseconds: 300),
+                              curve: Curves.ease,
+                            );
+                          }
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          height: 52.h,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(32),
+                            color: WbColors.blue009AFF,
+                          ),
+                          child: Center(
+                            child: Text(
+                              currantPage == widget.model.podr.length - 1
+                                  ? 'Finish'
+                                  : 'Next',
+                              style: TextStyle(
+                                color: WbColors.white,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18.h,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(height: 10.h),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
